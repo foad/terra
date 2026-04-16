32fc7379033ca5d5f3d56e2b7cf7fe708bd37493
Dan Foad <dan@foad.dev>
2026-04-16 18:44:05 +0100

    sort out frontend deploy

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './styles/global.module.css'
+import './styles/global.css'
 import { App } from './app.tsx'
 
 createRoot(document.getElementById('root')!).render(
