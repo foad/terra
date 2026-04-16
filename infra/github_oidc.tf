3a49842851698f70d337bb7dcae4103261b82e58
Dan Foad <dan@foad.dev>
2026-04-16 22:34:09 +0100

    add github actions deploy flows

---
+++ infra/github_oidc.tf
@@ -0,0 +1,96 @@
+# GitHub OIDC provider
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1c58a3a8518e8759bf075b76b750d4f2df264fcd",
+  ]
+}
+
+# IAM role for GitHub Actions
+resource "aws_iam_role" "github_actions" {
+  name = "${var.project_name}-github-actions"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.github.arn
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+        }
+        StringLike = {
+          "token.actions.githubusercontent.com:sub" = "repo:foad/terra:*"
+        }
+      }
+    }]
+  })
+}
+
+# S3 access for frontend deployment
+resource "aws_iam_role_policy" "github_actions_s3" {
+  name = "s3-deploy"
+  role = aws_iam_role.github_actions.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "s3:PutObject",
+        "s3:DeleteObject",
+        "s3:ListBucket",
+        "s3:GetObject",
+      ]
+      Resource = [
+        aws_s3_bucket.frontend.arn,
+        "${aws_s3_bucket.frontend.arn}/*",
+      ]
+    }]
+  })
+}
+
+# CloudFront invalidation for frontend deployment
+resource "aws_iam_role_policy" "github_actions_cloudfront" {
+  name = "cloudfront-invalidate"
+  role = aws_iam_role.github_actions.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = "cloudfront:CreateInvalidation"
+      Resource = aws_cloudfront_distribution.frontend.arn
+    }]
+  })
+}
+
+# Lambda update for backend deployment
+resource "aws_iam_role_policy" "github_actions_lambda" {
+  name = "lambda-deploy"
+  role = aws_iam_role.github_actions.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "lambda:UpdateFunctionCode",
+        "lambda:GetFunction",
+      ]
+      Resource = [
+        aws_lambda_function.api.arn,
+        aws_lambda_function.photo_processor.arn,
+      ]
+    }]
+  })
+}
+
+output "github_actions_role_arn" {
+  value = aws_iam_role.github_actions.arn
+}
